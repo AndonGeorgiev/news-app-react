a999e28759a9ba930750c52f248d6e30e94b81f6
AndonGeorgiev <andon2110@gmail.com>
2022-07-28 20:33:25 +0300

    config index file and install nodemon

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const expressConfig = require('./config/express');
+const dbConfig = require('./config/mongoose');
 
 
 
 
 const app = express();
+expressConfig(app);
+app.get('/', (res, req) => {
+    req.json({ "test": "test" });
+    req.end();
+})
+dbConfig()
+.then(() =>{
+    app.listen(4000, ()=>{
+        console.log(`server running on http://localhost:4000`);
+    })
+})
+.catch((err) =>{
+    console.log(err);
+})
