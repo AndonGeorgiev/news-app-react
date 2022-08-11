51d39b43490e989f4a5c144b32f75778699aa535
AndonGeorgiev <andon2110@gmail.com>
2022-08-11 23:26:23 +0300

    add  router

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,25 @@
 
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Home from './components/home/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import NewsCatalog from './components/NewsCatalog/NewsCatalog';
 
 function App() {
     return (
         <>
+
             <Header />
-            <Home />
+            
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/news/bulgaria' element={<NewsCatalog tag = "Bulgaria"/>} />
+                <Route path='/news/world' element={<NewsCatalog tag = "World"/>} />
+                <Route path='/news/sport' element={<NewsCatalog tag = "Sport"/>} />
+            </Routes>
+
+            <Footer />
         </>
     )
 
