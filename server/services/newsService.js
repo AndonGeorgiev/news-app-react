10e8f9b578e4d95799416ac1c82f31e98ae6293b
AndonGeorgiev <andon2110@gmail.com>
2022-08-11 23:30:09 +0300

    add find by tag functionality in news services

--- server/services/newsService.js
+++ server/services/newsService.js
@@ -1,9 +1,11 @@
 const News = require('../models/newsModel');
 
-exports.getAll = () => News.find({});
+exports.getAll = (tag) => News.find({ tag: tag });
 
 exports.create = async(newsInfo) => {
     let newNews = await News.create(newsInfo);
+    console.log('test');
+
 
     return newNews;
 };
