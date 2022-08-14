fdae56bc7c19f3c4e7b19abba1ad5bd49c3137fd
AndonGeorgiev <andon2110@gmail.com>
2022-08-14 23:02:43 +0300

    add root element

--- app/src/App.js
+++ app/src/App.js
@@ -5,10 +5,11 @@ import Home from './components/home/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import NewsCatalog from './components/NewsCatalog/NewsCatalog';
+import NewsDetails from './components/NewsDetails/NewsDetails';
 
 function App() {
     return (
-        <>
+        <div className="root">
 
             <Header />
             
@@ -17,10 +18,11 @@ function App() {
                 <Route path='/news/bulgaria' element={<NewsCatalog tag = "Bulgaria"/>} />
                 <Route path='/news/world' element={<NewsCatalog tag = "World"/>} />
                 <Route path='/news/sport' element={<NewsCatalog tag = "Sport"/>} />
+                <Route path='/news/:id' element={<NewsDetails/>} />
             </Routes>
 
             <Footer />
-        </>
+        </div>
     )
 
 }
