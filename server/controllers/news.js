0a33e773a362501534d63fd3869ec3a62bc02ae7
AndonGeorgiev <andon2110@gmail.com>
2022-08-14 22:59:41 +0300

    add get news by query functionality

--- server/controllers/news.js
+++ server/controllers/news.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const { getAll, create } = require('../services/newsService');
+const { getByQuery, create, getTheLastOneNews, getOneById } = require('../services/newsService');
 const router = express.Router();
 
 const getNews = async function(req, res) {
-    let tag = req.query.tag;
-    console.log(tag);
-    const allNews = await getAll(tag);
-    console.log(allNews);
-    res.json(allNews);
+    let query = req.query.query;
+    let result;
+
+    if(query === 'lastOne'){
+        result = await getTheLastOneNews();
+    }else{
+        result = await getByQuery(query);
+    }
+    
+    res.json(result);
     res.end();
 }
+
+
 
 const createNews = async function(req, res) {
     const newsInformation = req.body;
@@ -19,7 +26,16 @@ const createNews = async function(req, res) {
 
 }
 
+const getNewsById = async function(req, res) {
+    const id = req.params.id;
+    const news= await getOneById(id);
+    res.json(news);
+    res.end();
+
+}
+
 router.get('/news', getNews)
+router.get('/news/:id', getNewsById)
 router.post('/news/create', createNews)
 
 
