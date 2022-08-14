193d01b4bb3b1dfd00aae7f96682936a52f6e572
AndonGeorgiev <andon2110@gmail.com>
2022-08-14 22:59:02 +0300

    add getByQuery getOneById and getTheLastOneNews functions

--- server/services/newsService.js
+++ server/services/newsService.js
@@ -1,6 +1,13 @@
 const News = require('../models/newsModel');
 
-exports.getAll = (tag) => News.find({ tag: tag });
+exports.getByQuery = (tag) => News.find({ tag: tag });
+exports.getOneById = (id) => News.findById(id);
+
+exports.getTheLastOneNews = async function(){
+   let allNews = await News.find({});
+   let lastOne = allNews[allNews.length - 1];
+   return lastOne
+}
 
 exports.create = async(newsInfo) => {
     let newNews = await News.create(newsInfo);
@@ -9,3 +16,4 @@ exports.create = async(newsInfo) => {
 
     return newNews;
 };
+
